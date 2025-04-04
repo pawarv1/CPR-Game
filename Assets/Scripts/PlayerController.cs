7fb1614a0400a104aaa8b54097f13fe0a8171419
Matthew0314 <matthewh314@gmail.com>
2025-04-04 19:15:10 -0400

    Allows player to move forward based on where camera is looking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Vector2 moveInput;
     private PlayerInput playerInput;
+    [SerializeField] Transform headset;       // Reference to the VR camera (head)
+    [SerializeField] CharacterController controller; // Or you can use transform.Translate
+    private float speed = 2f;
 
     
     void Start()
@@ -20,24 +23,36 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        float horizontalInput = moveInput.x;
-        float verticalInput = moveInput.y;
-        animator.SetFloat("Blend",horizontalInput);
-        animator.SetFloat("BlendY", verticalInput);
+        // float horizontalInput = moveInput.x;
+        // float verticalInput = moveInput.y;
+        // animator.SetFloat("Blend",horizontalInput);
+        // animator.SetFloat("BlendY", verticalInput);
+
+        Vector3 headForward = headset.forward;
+        Vector3 headRight = headset.right;
+
+        headForward.y = 0f;
+        headRight.y = 0f;
+        headForward.Normalize();
+        headRight.Normalize();
         
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        // Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+
+        Vector3 move = headForward * moveInput.y + headRight * moveInput.x;
+
+        controller.Move(move * speed * Time.deltaTime);
         
-        if (movement.magnitude > 0)
-        {
-            movement.Normalize();
-        }
+        // if (movement.magnitude > 0)
+        // {
+        //     movement.Normalize();
+        // }
         
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        // transform.Translate(movement * moveSpeed * Time.deltaTime);
         
-        float movementMagnitude = movement.magnitude;
-        animator.SetFloat("Speed", movementMagnitude);
+        // float movementMagnitude = movement.magnitude;
+        // animator.SetFloat("Speed", movementMagnitude);
         
-        if (Input.GetMouseButtonDown(0))
+        if (playerInput.actions["Action"].WasPressedThisFrame())
         {
             animator.SetTrigger("isGivingCPR");
         }
