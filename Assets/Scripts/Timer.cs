243d9e054ce5466373fbb283e366ea8e16255503
toothsocket <difabii1@tcnj.edu>
2025-04-24 08:55:49 -0400

    Added Bystanders to tutorial level

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
@@ -8,17 +9,26 @@ public class Timer : MonoBehaviour
     public GameObject spawnPoint;
     public GameObject stoppingPoint;
     public Quaternion spawnRotation;
+    public GameObject firstByStander;
+    public GameObject secondByStander;
     private float timer = 0f;
-    private float timeTillEMTS = 10f; 
+    private float timeTillEMTS = 30f; 
     private bool ambulanceInstantiated = false;
     private GameObject instantiatedAmbulance;
     private float moveSpeed = 0.1f; // Speed at which the ambulance will move (between 0 and 1)
+     private float timeToActivateB = 10f;
+    private bool BsActivated = false;
+    public string scared = "Scared";
+    public string yell = "Yell";
+    public GameObject button;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Time remaining: " + (timeTillEMTS - timer));
         spawnRotation = Quaternion.Euler(0f, 90f, 0f);
+        if (firstByStander != null) firstByStander.SetActive(false);
+        if (secondByStander != null) secondByStander.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,7 +36,10 @@ public class Timer : MonoBehaviour
     {
         timer += Time.deltaTime;
         Debug.Log("Time remaining: " + (timeTillEMTS - timer));
-
+        if (timer >= timeToActivateB && !BsActivated)
+        {
+            ActivateObjectsWithAnimation();
+        }
         if (timer >= timeTillEMTS && !ambulanceInstantiated)
         {
             instantiatedAmbulance = Instantiate(ambulance, spawnPoint.transform.position, spawnRotation);
@@ -44,6 +57,51 @@ public class Timer : MonoBehaviour
             }
         }
     }
+     private void ActivateObjectsWithAnimation()
+    {
+        if (firstByStander != null)
+        {
+            firstByStander.SetActive(true);
+            Animator firstAnimator = firstByStander.GetComponent<Animator>();
+            if (firstAnimator != null)
+            {
+                firstAnimator.SetTrigger(yell);
+            }
+        }
+
+        if (secondByStander != null)
+        {
+            secondByStander.SetActive(true);
+            Animator secondAnimator = secondByStander.GetComponent<Animator>();
+            if (secondAnimator != null)
+            {
+                secondAnimator.SetTrigger(scared);
+            }
+        }
+
+        if (button != null)
+        {
+            button.SetActive(true);
+        }
+        
+        BsActivated = true;
+    }
+
+    public void DeleteBystanders()
+    {
+            if (firstByStander != null)
+            {
+                Destroy(firstByStander);
+                firstByStander = null;
+            }
+            
+            if (secondByStander != null)
+            {
+                Destroy(secondByStander);
+                secondByStander = null;
+            }
+    }
+        
 }
 
 
