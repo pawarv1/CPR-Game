54558c57b11a89ed44187efaba5ef7c522e30bbc
toothsocket <difabii1@tcnj.edu>
2025-04-30 22:28:43 -0400

    Bystander logic and text working

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class Timer : MonoBehaviour
 {
@@ -9,38 +10,65 @@ public class Timer : MonoBehaviour
     public GameObject spawnPoint;
     public GameObject stoppingPoint;
     [SerializeField] public Quaternion spawnRotation;
-     [SerializeField] public AudioClip ambulanceSirenClip;
+    [SerializeField] public AudioClip ambulanceSirenClip;
     public GameObject firstByStander;
     public GameObject secondByStander;
     private float timer = 0f;
     private float timeTillEMTS = 30f; 
     private bool ambulanceInstantiated = false;
     private GameObject instantiatedAmbulance;
-    private float moveSpeed = 0.1f; // Speed at which the ambulance will move (between 0 and 1)
-     private float timeToActivateB = 10f;
+    private float moveSpeed = 0.1f;
+    private float timeToActivateB = 10f;
     private bool BsActivated = false;
     public string scared = "Scared";
     public string yell = "Yell";
     public GameObject button;
+    
+    private List<InputDevice> inputDevices = new List<InputDevice>();
+    private bool bystandersActive = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Time remaining: " + (timeTillEMTS - timer));
-        //spawnRotation = Quaternion.Euler(0f, 90f, 0f);
         if (firstByStander != null) firstByStander.SetActive(false);
         if (secondByStander != null) secondByStander.SetActive(false);
+        
+        InputDevices.GetDevices(inputDevices);
+        foreach (var device in inputDevices)
+        {
+            if (device.characteristics.HasFlag(InputDeviceCharacteristics.Controller))
+            {
+                Debug.Log("Found controller: " + device.name);
+            }
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
         Debug.Log("Time remaining: " + (timeTillEMTS - timer));
+        
+        if (bystandersActive)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                DeleteBystanders();
+            }
+            
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                DeleteBystanders();
+            }
+            
+            CheckOculusControllerInput();
+        }
+        
         if (timer >= timeToActivateB && !BsActivated)
         {
             ActivateObjectsWithAnimation();
+            bystandersActive = true;
         }
+        
         if (timer >= timeTillEMTS && !ambulanceInstantiated)
         {
             instantiatedAmbulance = Instantiate(ambulance, spawnPoint.transform.position, spawnRotation);
@@ -66,7 +94,40 @@ public class Timer : MonoBehaviour
             instantiatedAmbulance.transform.position = Vector3.Lerp(instantiatedAmbulance.transform.position, stoppingPoint.transform.position, step);
         }
     }
-     private void ActivateObjectsWithAnimation()
+    
+    private void CheckOculusControllerInput()
+    {
+        if (inputDevices.Count == 0)
+        {
+            InputDevices.GetDevices(inputDevices);
+        }
+        
+        foreach (var device in inputDevices)
+        {
+            if (device.characteristics.HasFlag(InputDeviceCharacteristics.Controller))
+            {
+                bool primaryButtonPressed = false;
+                bool secondaryButtonPressed = false;
+                
+                device.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonPressed);
+                device.TryGetFeatureValue(CommonUsages.secondaryButton, out secondaryButtonPressed);
+                
+                if (primaryButtonPressed)
+                {
+                    DeleteBystanders();
+                    return;
+                }
+                
+                if (secondaryButtonPressed)
+                {
+                    DeleteBystanders();
+                    return;
+                }
+            }
+        }
+    }
+    
+    private void ActivateObjectsWithAnimation()
     {
         if (firstByStander != null)
         {
@@ -98,19 +159,20 @@ public class Timer : MonoBehaviour
 
     public void DeleteBystanders()
     {
-            if (firstByStander != null)
-            {
-                Destroy(firstByStander);
-                firstByStander = null;
-            }
-            
-            if (secondByStander != null)
-            {
-                Destroy(secondByStander);
-                secondByStander = null;
-            }
-    }
+        if (firstByStander != null)
+        {
+            Destroy(firstByStander);
+            firstByStander = null;
+        }
         
+        if (secondByStander != null)
+        {
+            Destroy(secondByStander);
+            secondByStander = null;
+        }
+        
+        bystandersActive = false;
+    }
 }
 
 
